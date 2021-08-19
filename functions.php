b4e55b757062b3b6c96091160c018b2d0fc88974
Fernando Soares <fernandosoares.dev@gmail.com>
2021-08-19 11:25:30 -0300

    Initial Commit w/ BS5

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Assets
+ */
+function cisf_enqueue_resources() {
+    wp_enqueue_style('bootstrap-5-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap-5-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', ['jquery'], false, true);
+}
+add_action('wp_enqueue_scripts', 'cisf_enqueue_resources');

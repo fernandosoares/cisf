8c674f166b353f134cdd9b3140fde00b96f1b777
Fernando Soares <fernandosoares.dev@gmail.com>
2021-09-14 15:36:11 -0300

    Live Link

--- views/live.php
+++ views/live.php
@@ -5,7 +5,7 @@
         </div>
     </div>
     <div id="live-slides">
-        <a href="" target="_blank"><img src="<?= get_template_directory_uri() . '/assets/img/ABBC_ESTAMOS_AO_VIVO_NOVA.jpg'?>" alt="SLIDE" /></a>
+        <a href="https://www.youtube.com/watch?v=1zNWvgBtI4c" target="_blank"><img src="<?= get_template_directory_uri() . '/assets/img/ABBC_ESTAMOS_AO_VIVO_NOVA.jpg'?>" alt="SLIDE" /></a>
         <a href="https://youtu.be/1zNWvgBtI4c" target="_blank"><img src="<?= get_template_directory_uri() . '/assets/img/ABBC_ESG_NOVA.jpg'?>" alt="SLIDE" /></a>
         <a href="https://youtu.be/xvbSMJZVQks" target="_blank"><img src="<?= get_template_directory_uri() . '/assets/img/ABBC_CIBERSEGURANCA_NOVA.jpg'?>" alt="SLIDE" /></a>
         <a href="https://youtu.be/WuZKSvvrjTQ" target="_blank"><img src="<?= get_template_directory_uri() . '/assets/img/ABBC_OPEN_FINANCE_NOVA.jpg'?>" alt="SLIDE" /></a>
